a4ee6c71babce8b88b6689e63ea8d685648a24a0
THORISO <220674868@ump.ac.za>
2025-05-12 11:23:50 +0200

    NetBeans to GitHub

---
+++ src/main/java/com/mycompany/test_2/Test_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test_2;
+
+/**
+ *
+ * @author lenah
+ */
+public class Test_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
